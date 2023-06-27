2965764f9925bb701ce870f87212184c4db6afcb
Benjamin Hamada <benjaminhamada@yahoo.fr>
2023-06-27 15:43:20 +0700

    fixed issue #2 - form input validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,31 +1,30 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const modalCloseBtn = document.querySelector(".close");
+const modalCloseTopBtn = document.querySelector(".close");
+const modalCloseBtn = document.querySelector(".close-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalCloseBtn.addEventListener("click", closeModal)
-
+modalCloseTopBtn.addEventListener("click", closeModal);
+modalCloseBtn.addEventListener("click", closeModal);
 
 // close modal form
 function closeModal() {
-  modalbg.style.display = "none";
+	modalbg.style.display = "none";
 }
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+	modalbg.style.display = "block";
 }
-
-
